4044a6e6cb686435471b0a7fabdb887f1720cfe2
Salau Ibrahim <officialsalau@gmail.com>
2023-11-30 17:13:13 +0100

    Hides html elements and deleted few console.log statements in js file

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const accessKey = "2uDvpyTQEYGQ6GQBzw7hqe-aRX0pdoNGfEYgNfEDdEk";
+const accessKey = "2uDvpyTQEYGQ6GQBzw7hqe-aRX0pdoNGfEYgNfEDdEk"; /*API Key from unsplash.com*/
 
 const formElement = document.querySelector("form");
 const searchInputElement = document.getElementById("search-input");
 const searchResultsElement = document.querySelector(".search-results");
-const showMoreButton = document.getElementById("show-more-button");
+const showMoreButtonElement = document.getElementById("show-more-button");
 
 let inputData = "";
 let page = 1;
@@ -11,13 +11,12 @@ let page = 1;
 async function searchImages(){
     inputData = searchInputElement.value;
     const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
-    console.log(url);
     const response = await fetch(url);
     const data = await response.json();
+
         if(page === 1){
             searchResultsElement.innerHTML = ""; 
         }
-
         const results = data.results;
 
         results.map((results)=>{
@@ -36,13 +35,19 @@ async function searchImages(){
             searchResultsElement.appendChild(imageWrapper);
         });
 
+        page++;
+
         if(page > 1){
-            showMoreButton.style.display = block;
+            showMoreButtonElement.style.display = "block";
         }
 };
 
 formElement.addEventListener("submit", (event)=>{
     event.preventDefault();
     page = 1;
+    searchImages();
+});
+
+showMoreButtonElement.addEventListener("click", ()=>{
     searchImages();
 });
